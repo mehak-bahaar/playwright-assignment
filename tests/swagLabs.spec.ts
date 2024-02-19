63d157a24f7a7534115e1f10c5dd572a40229697
Mehak <mehakbahar4@gmail.com>
2024-02-18 19:44:00 -0800

    removed unnecessary files

--- tests/swagLabs.spec.ts
+++ tests/swagLabs.spec.ts
@@ -30,25 +30,3 @@ test.describe(()=>{
         await page.close()
     })
 })
-//login
-// await page.goto('https://www.saucedemo.com/v1/');
-// await page.locator('[data-test="username"]').click();
-// await page.locator('[data-test="username"]').fill('standard_user');
-// await page.locator('[data-test="password"]').click();
-// await page.locator('[data-test="password"]').fill('secret_sauce');
-// await page.getByRole('button', { name: 'LOGIN' }).click();
-
-//cart
-// await page.locator('div').filter({ hasText: /^\$29\.99ADD TO CART$/ }).getByRole('button').click();
-// await page.getByRole('link', { name: '1' }).click();
-
-//checkout
-// await page.getByRole('link', { name: 'CHECKOUT' }).click();
-// await page.locator('[data-test="firstName"]').click();
-// await page.locator('[data-test="firstName"]').fill('Jhon');
-// await page.locator('[data-test="lastName"]').click();
-// await page.locator('[data-test="lastName"]').fill('Tame');
-// await page.locator('[data-test="postalCode"]').click();
-// await page.locator('[data-test="postalCode"]').fill('38000');
-// await page.getByRole('button', { name: 'CONTINUE' }).click();
-// await page.getByRole('link', { name: 'FINISH' }).click();
